34738e89b1f85f369a3eeeb9456c7b77070dce5d
Adam Horn <adamhorn8@gmail.com>
2018-06-06 09:43:12 -0400

    second commit

--- Lab26/App_Start/BundleConfig.cs
+++ Lab26/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Lab26
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
